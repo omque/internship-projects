454c7cd13e9a42055565d7a59d17fe8ba416a50e
omque <4947796+omque@users.noreply.github.com>
2021-04-05 00:50:09 -0700

    Added Sachacks 2021

---
+++ sachacks2021/constants.py
@@ -0,0 +1,19 @@
+# Program/File name: constants.py
+# Author: Omair Qazi
+# Date: 2021/02/20
+#
+# Purpose: Define constants for import_lyrics.py
+#
+# Description:
+#
+# Date Modified:
+# Mod Author: 
+# Mod Reason:
+
+#Define file output constant.
+LYRICS_FILE = "../outputLyrics.txt"
+
+
+
+
+
